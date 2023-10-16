ce340b17df8bad93d199ca558163daac04391fb0
SHANA SHABNAM NASIM <142229568+shananasim16@users.noreply.github.com>
2023-10-16 12:52:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 function getFormvalue() {
-    //Write your code here
-
+    // Get the first name input element by its name
+    const firstNameInput = document.getElementsByName("firstName")[0];
+    
+    // Get the last name input element by its name
+    const lastNameInput = document.getElementsByName("lastName")[0];
+    
+    // Get the values of the input elements
+    const firstName = firstNameInput.value;
+    const lastName = lastNameInput.value;
+    
+    // Display the values using alert
+    alert("First Name: " + firstName + "\nLast Name: " + lastName);
 }
