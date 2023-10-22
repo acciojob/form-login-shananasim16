b402c59c39c7174d2be814d26b71114533c21254
SHANA SHABNAM NASIM <142229568+shananasim16@users.noreply.github.com>
2023-10-22 23:25:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
-// script.js
-document.getElementById('myForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent the form from submitting the traditional way
-
-    // Get the values from the form
+function showName() {
     var firstName = document.getElementById('firstName').value;
     var lastName = document.getElementById('lastName').value;
 
-    // Display the values using the alert function
+
     alert("First Name: " + firstName + "\nLast Name: " + lastName);
 });
-
